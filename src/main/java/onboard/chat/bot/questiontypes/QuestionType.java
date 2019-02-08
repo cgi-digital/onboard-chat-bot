3a401469610f7a0a3b1f94a8b30b93521a05d7a2
jamesrloveday <jamesrloveday@outlook.com>
2019-02-08 10:10:32 +0000

    Improved the resource file checks and added question response for sick

--- src/main/java/onboard/chat/bot/questiontypes/QuestionType.java
+++ src/main/java/onboard/chat/bot/questiontypes/QuestionType.java
@@ -1,15 +1,15 @@
 package onboard.chat.bot.questiontypes;
 
 import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.methods.SlackApiException;
 import com.github.seratch.jslack.api.model.Channel;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import onboard.chat.bot.IncomingMessage;
 import onboard.chat.bot.response.ResponseHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.IOException;
@@ -19,16 +19,12 @@ import static java.util.Arrays.asList;
 @Slf4j
 public class QuestionType {
 
-
-    @Autowired
-    private ResourceLoader resourceLoader;
-
-    public void filterAndRespond(Slack slack, IncomingMessage message, Channel channel, String incomingText) throws IOException {
+    public void filterAndRespond(Slack slack, IncomingMessage message, Channel channel, String incomingText) {
         val resource = new File("./resources/resources.xml").getAbsoluteFile();
 
         ClassLoader classLoader = getClass().getClassLoader();
         val resourcesHandler =
-                (resource != null)
+                (resource.canRead())
                         ? resource : new File(classLoader.getResource("resources.xml").getFile());
         try {
 
@@ -36,10 +32,10 @@ public class QuestionType {
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
             val resources = (Resources) unmarshaller.unmarshal(resourcesHandler);
             val templates = asList(resources.getTemplate());
-            for(Template t: templates) {
+            for (Template t : templates) {
                 val textList = asList(t.getQuestion().getText());
-                for(String s: textList) {
-                    if(incomingText.toLowerCase().contains(s)) {
+                for (String s : textList) {
+                    if (incomingText.toLowerCase().contains(s)) {
                         log.info("The response to print is {}", t.getResponse());
                         ResponseHandler.sendReply(slack, message, channel, t.getResponse());
                         break;
@@ -47,7 +43,11 @@ public class QuestionType {
                 }
             }
 
-        } catch (Exception ex) {
+        }catch (IOException ex) {
+                log.info("Exception: {}", ex.getMessage());
+        }catch (SlackApiException ex) {
+            log.info("Exception: {}", ex.getMessage());
+        } catch (JAXBException ex) {
             log.info("Exception: {}", ex.getMessage());
         }
 
